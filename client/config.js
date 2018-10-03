45bc3ee879c7076c885ab13337b2d8e7bd621f34
gXuanN <tlfggx@gmail.com>
2018-10-03 18:29:09 +0800

    首页 :ok_hand:

--- client/config.js
+++ client/config.js
@@ -3,26 +3,71 @@
  */
 
 // 此处主机域名修改成腾讯云解决方案分配的域名
-var host = 'https://123456.qcloud.la';
+var host = 'https://rdazgryz.qcloud.la';
 
 var config = {
 
-    // 下面的地址配合云端 Demo 工作
-    service: {
-        host,
+  // 下面的地址配合云端 Demo 工作
+  service: {
+    host,
 
-        // 登录地址，用于建立会话
-        loginUrl: `${host}/weapp/login`,
+    // 登录地址，用于建立会话
+    loginUrl: `${host}/weapp/login`,
 
-        // 测试的请求地址，用于测试会话
-        requestUrl: `${host}/weapp/user`,
+    // 测试的请求地址，用于测试会话
+    requestUrl: `${host}/weapp/user`,
 
-        // 测试的信道服务地址
-        tunnelUrl: `${host}/weapp/tunnel`,
+    // 拉取用户信息
+    user: `${host}/weapp/user`,
 
-        // 上传图片接口
-        uploadUrl: `${host}/weapp/upload`
-    }
+    // 拉取电影信息
+    hotMovies: `${host}/weapp/hotMovies`,
+
+    // 拉去电影详情
+    moviesDetail: `${host}/weapp/moviesDetail/`,
+
+    // 拉去热门电影列表
+    moviesList: `${host}/weapp/moviesList`,
+
+    // 拉去电影评价列表
+    commentsListItem: `${host}/weapp/commentsListItem`,
+
+    // 提交电影评价
+    addComment: `${host}/weapp/addComment`,
+
+    // 获取电影评价
+    getComment: `${host}/weapp/getComment`,
+
+    // 收藏电影
+    setCollect: `${host}/weapp/addCollect`,
+
+    // 查询是否收藏
+    noCollect: `${host}/weapp/noCollect`,
+
+    // 查询是否评论 
+    noaCollect: `${host}/weapp/noaCollect`,
+
+    // 取消收藏
+    cancCollect: `${host}/weapp/cancCollect`,
+
+    // 电影收藏列表
+    getCollectList: `${host}/weapp/getCollectList`,
+
+    // 影评详情
+    getCommDetails: `${host}/weapp/getCommDetails`,
+
+    // 电影评价列表
+    getComments: `${host}/weapp/getComments`,
+
+    // 电影评价列表
+    getUserComments: `${host}/weapp/getUserComments`,
+
+    // 已发布列表
+    getUserReported: `${host}/weapp/getUserReported`,
+    // 上传接口
+    uploadUrl: `${host}/weapp/upload`,
+
+  }
 };
 
 module.exports = config;
---
+++ client/pages/index/index.js
@@ -0,0 +1,173 @@
+// pages/user/user.js
+const app = getApp()
+const qcloud = require('../../vendor/wafer2-client-sdk/index.js')
+const config = require('../../config.js')
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    userInfo: null,
+    locationAuthType: app.data.locationAuthType,
+    getComment: null,
+    gethotmovie: {}
+  },
+  onPullDownRefresh() {
+    this.getMovieLists(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+  homeHot() {
+    // url = "/pages/hot/hot" 
+    wx.navigateTo({
+      url: '/pages/hot/hot',
+    })
+  },
+  homeUser() {
+    // url = "/pages/user/user" 
+    wx.navigateTo({
+      url: '/pages/user/user',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    // console.log(!this.data.userInfo)
+    // if (!this.data.userInfo) return;
+    this.getMovieLists()
+    // console.log(this.data.getComment)
+    // console.log(!this.data.getComment)
+    // console.log(!!this.data.getComment)
+  },
+  topDetail() {
+    // url = "/pages/top-detail/top-detail"
+    let getMovieId = this.data.gethotmovie.id
+    let getMovieTitle = this.data.gethotmovie.title
+    let getMovieImage = this.data.gethotmovie.image
+
+    let getListComm = this.data.getComment
+    let getCommentHead = getListComm.avatar
+    let getCommentName = getListComm.username
+    let getCommentContent = getListComm.content
+    let getCommentOpenId = this.data.userInfo.openId
+    let getCmt = getListComm.cmt
+    let getVideo = getListComm.video
+    if (!!getListComm) {
+      if (getMovieId) {
+        wx.navigateTo({
+          url: `/pages/top-detail/top-detail?id=${getMovieId}&title=${getMovieTitle}&image=${getMovieImage}&avatar=${getCommentHead}&username=${getCommentName}&content=${getCommentContent}&openId=${getCommentOpenId}&cmt=${getCmt}&video=${getVideo}`,
+        })
+
+      } else {
+        wx.showToast({
+          title: '页面失效',
+        })
+      }
+    } else {
+      console.log(getListComm)
+    }
+  },
+  hometap() {
+    let getMovieId = this.data.gethotmovie.id
+    let getMovieTitle = this.data.gethotmovie.title
+    let getMovieCategory = this.data.gethotmovie.category
+    let getMovieImage = this.data.gethotmovie.image
+    let getMovieDescription = this.data.gethotmovie.description
+    wx.navigateTo({
+      url: `/pages/mv-detail/mv-detail?id=${getMovieId}&title=${getMovieTitle}&category=${getMovieCategory}&image=${getMovieImage}&description=${getMovieDescription}`,
+    })
+  },
+  getComment(id) {
+    qcloud.request({
+      url: config.service.getComment,
+      data: {
+        comment_id: id
+      },
+      success: result2 => {
+        if (!result2.data.code) {
+          this.setData({
+            getComment: result2.data.data
+          })
+        }
+      },
+      fail: result2 => {
+        wx.showToast({
+          title: '评论数据加载失败',
+        })
+      }
+    })
+  },
+  getMovieLists(callback) {
+    wx.showLoading({
+      title: '数据加载中',
+    })
+    qcloud.request({
+      url: config.service.hotMovies,
+      success: result => {
+        wx.hideLoading()
+        if (!result.data.code && !!result.data.data) {
+          this.setData({
+            gethotmovie: result.data.data[0]
+          })
+          this.getComment(result.data.data[0].id)
+
+        } else {
+          wx.showToast({
+            title: '热门电影加载失败',
+          })
+        }
+        // console.log("首页数据：")
+        // console.log(result.data.data[0].id)
+      },
+      fail: result => {
+        wx.hideLoading()
+        wx.showToast({
+          title: '数据加载失败',
+        })
+        console.log(result)
+      },
+      complete: () => {
+        typeof callback === 'function' && callback()
+        // callback && callback()
+      }
+    })
+  },
+  onTapLogin: function () {
+    app.login({
+      success: ({ userInfo }) => {
+        this.setData({
+          userInfo,
+          locationAuthType: app.data.locationAuthType
+        })
+        // 登录成功进入主页
+        this.getMovieLists()
+
+      },
+      error: () => {
+        this.setData({
+          locationAuthType: app.data.locationAuthType
+        })
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    // 同步授权状态
+    this.setData({
+      locationAuthType: app.data.locationAuthType
+    })
+    app.checkSession({
+      success: ({ userInfo }) => {
+        this.setData({
+          userInfo
+        })
+      }
+    })
+  },
+
+})
